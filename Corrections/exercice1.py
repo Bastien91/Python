3e4d7a1a675469813fef969f63e4b6f4ddd37077
Bastien <125685606+Bastien91@users.noreply.github.com>
2025-06-28 14:47:41 +0200

    Update exercice1.py

--- Corrections/exercice1.py
+++ Corrections/exercice1.py
@@ -1,6 +1,9 @@
 
 # Exercice 1 : Affichage age
-
+"""
+Faire un programme qui affiche ton âge : 
+  - tu rentre ta date de naissance et le programme doit afficher ton âge
+"""
 annee_actuelle = 2025
 annee_naissance = int(input("Saisir ton année de naissance : "))
 
