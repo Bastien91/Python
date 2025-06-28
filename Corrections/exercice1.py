7c51e8c12ba16569e78f24712ee19c379a039859
Bastien <125685606+Bastien91@users.noreply.github.com>
2025-06-28 14:33:07 +0200

    Update exercice1.py

---
+++ Corrections/exercice1.py
@@ -0,0 +1,9 @@
+
+# Exercice 1 : Affichage age
+
+annee_actuelle = 2025
+annee_naissance = int(input("Saisir ton année de naissance : "))
+
+age = annee_actuelle - annee_naissance
+
+print("Tu as "+ str(age) +" ans")
