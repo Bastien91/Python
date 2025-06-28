92f40ca013a3d9dcd10127b97ef4a03bda98e6c4
Bastien <125685606+Bastien91@users.noreply.github.com>
2025-06-28 14:59:39 +0200

    Create exercice3.py

---
+++ Corrections/exercice3.py
@@ -0,0 +1,17 @@
+# Exercice 3 : Bob et ses billes
+"""
+Bob possède un certain nombre de billes. Il veut regrouper ses billes dans des sacs, en mettant 17 billes par sac.
+Écrire un algorithme qui :
+    - demande de saisir un nombre de billes,
+    - affiche le nombre de sacs qui seront pleins,
+    - affiche le nombre de billes restantes.
+"""
+
+nombre_de_billes = int(input("Saisir le nombre de billes de Bob : "))
+capcitee_du_sac = 17
+
+nombre_sac_plein = nombre_de_billes // capcitee_du_sac
+nombre_de_billes_restantes = nombre_de_billes % capcitee_du_sac
+
+print("Nombre de sac plein : "+ str(nombre_sac_plein))
+print("il reste "+ str(nombre_de_billes_restantes) +" billes")
